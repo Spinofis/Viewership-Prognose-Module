77db932707c9b98b0b2ed5fcaef80a78c4874d91
unknown <bartek2560@gmail.com>
2019-03-01 17:52:42 +0100

    start prediction, change database script

--- Prediction/prediction.py
+++ Prediction/prediction.py
@@ -1,1 +1,15 @@
-import tensorflow as tf
+import pyodbc
+import pandas as pd
+
+
+def get_database_connection():
+    return pyodbc.connect('Driver={SQL Server};'
+                          'Server=DESKTOP-12G6OSC\BARTEK;'
+                          'Database=ViewershipForecastDB;'
+                          'Trusted_Connection=yes;')
+
+
+def get_data_from_db():
+    sql = "select * from [dbo].[f_p_grp_hours_pivot_get] () where month(date) in (1)"
+    df = pd.read_sql(sql, get_database_connection())
+    return df
